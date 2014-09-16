35a5a03b9ef0afb94c292132355e9fa405e3afd2
sakalli <notjustsilicon@gmail.com>
2014-09-16 22:10:47 +0300

    first version, think this works

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## clearing inverse cache as it might have been previously used
+        cache <- NULL
+        ## set function
+        set <- function(y) {
+                ## setting value to x in the parent environment
+                x <<- y
+                ## clearing inverse chache as we now have a new matrix
+                cache <<- NULL 
+        }
+        ## get function returns the matrix stored
+        get <- function() x
+        ## setinverse function sets cache variable in parent environment
+        setinverse <- function(inverse) cache <<- inverse
+        ## getinverse function sets cache variable in parent environment        
+        getinverse <- function() cache
+        ## return a list with all functions
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
@@ -11,5 +30,21 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## begin by getting inverse from makeCacheMatrix function
+        cache <- x$getinverse()
+        ## checking if chache exists
+        if(!is.null(cache)) {
+                ## chache exists...
+                message("getting cached data")
+                ## ...so return cache and exit function
+                return(cache)
+        }
+        ## we've reached here which means cache was empty so we get the matrix
+        matrix <- x$get()
+        ## doing the solve calculation and assign it to cache
+        cache <- solve(matrix, ...)
+        ## set the inverse cache in makeCacheMatrix environment
+        x$setinverse(cache)
+        ## return the cache
+        cache
 }
